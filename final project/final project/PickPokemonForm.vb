0036916f4983517aaad3c9d2f168f97a10aa31c4
alberto morales <moraaa01@students.ipfw.edu>
2017-11-27 20:55:35 -0500

    finished implementing requirement #2 only allowing you to choose 6 pokemon.

--- final project/final project/PickPokemonForm.vb
+++ final project/final project/PickPokemonForm.vb
@@ -1,6 +1,7 @@
 ﻿Option Strict On
 Option Explicit On
 Public Class PickPokemonForm
+    Dim index As Integer = 0
     Private mPokemon As New Pokemon
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Me.Close()
@@ -17,22 +18,17 @@ Public Class PickPokemonForm
     End Sub
 
     Private Sub btnAddPokemon_Click(sender As Object, e As EventArgs) Handles btnAddPokemon.Click
-        'Dim index As Integer
-        'If index < 6 Then
-        '    Do While index < 6
-        '        index += 1
-        '        If dgvPickPokemon.SelectedRows.Count > 0 Then
-        '            Dim dexID As Short = CShort(dgvPickPokemon.SelectedRows(0).Cells(0).Value)
-        '            If mPokemon.Delete(dexID) Then
-        '                dgvPickPokemon.DataSource = mPokemon.Items
-        '            Else
-        '                MessageBox.Show("could not delete pokemon")
-        '            End If
-        '        End If
-        '    Loop
-        'Else
-        '    MessageBox.Show("hi")
-        'End If
+        If index < 6 Then
+            index += 1
+            If dgvPickPokemon.SelectedRows.Count > 0 Then
+                Dim dexID As Short = CShort(dgvPickPokemon.SelectedRows(0).Cells(0).Value)
+                If mPokemon.Delete(dexID) Then
+                    dgvPickPokemon.DataSource = mPokemon.Items
+                Else
+                    MessageBox.Show("could not delete pokemon")
+                End If
+            End If
+        End If
     End Sub
 
     Private Sub btnNEWPKMNINSTANCE_Click(sender As Object, e As EventArgs) Handles btnNEWPKMNINSTANCE.Click
